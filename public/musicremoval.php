9e0c354b540796894847189254850a4f0ff842b6
devricardorodrigues <ricardoprdecarvalho@gmail.com>
2025-09-16 23:04:00 -0300

    Revert "Implementação do codigo base e de teste da função de remover musicas"

--- public/musicremoval.php
+++
@@ -1,7 +0,0 @@
-<?php
-// public/musicremoval.php
-// Visualizador público para a UI do modal (sem alterações de banco).
-require_once __DIR__ . '/../app/Controllers/MusicRemovalController.php';
-$controller = new \App\Controllers\MusicRemovalController();
-$songs = $controller->getPendingSongs(); // dados mock genéricos
-include __DIR__ . '/../app/Views/musicremoval.php';
