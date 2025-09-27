817e3175574f6a63971e521780ddc0b2aac32d40
FelixSJ1 <felix.joao@mail.uft.edu.br>
2025-09-27 09:50:30 -0300

    Modificacao do banco de dados mais atual

--- app/Controllers/ControllerF.php
+++ app/Controllers/ControllerF.php
@@ -1,5 +1,9 @@
 <?php
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+require_once __DIR__ . "/../config/conector.php";
+>>>>>>> Stashed changes
 =======
 require_once __DIR__ . "/../config/conector.php";
 >>>>>>> Stashed changes
